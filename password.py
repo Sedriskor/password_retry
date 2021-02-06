5f91605e000db9268b48dba94e224b2b088a9855
Sedriskor <comodexit@gmail.com>
2021-02-06 18:53:05 +0800

    ver.3

--- password.py
+++ password.py
@@ -6,14 +6,16 @@ password = 'a123456'
 x = 3 #可試次數
 
 while x > 0:
+    x = x - 1
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功!!')
         break #stop
     else:
-        x = x - 1
-        print('輸入錯誤 還有', x, '次機會')
-        print('嘗試次數已達上限')
+        if x > 0:
+            print('密碼錯誤,還有', x, '次機會')
+        else :
+            print('嘗試次數已達上限')
 
  
  
